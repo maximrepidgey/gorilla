3347d68acb75a8b94f32557debb91b3bc7ff9a4c
maximrepidgey <maxim.ryakhovskiy@epfl.ch>
2023-10-02 09:24:52 +0200

    add data processing for mosaic

--- training/data-prep.py
+++ training/data-prep.py
@@ -22,8 +22,7 @@ def generate_data(filename, write):
                 line = json.loads(line)
                 line = line["code"].split(sep)
                 # instruction = line[0].split("Instruction': ")[1] # extract the instruction
-                instruction = line[0][1:] # extract the instruction and take away unwanted char
-
+                instruction = line[0]
                 # after splitting, try to extract the output, if the string is malformed, ignore it
                 try:
                     output = line[1]
@@ -38,15 +37,67 @@ def generate_data(filename, write):
         print("Total samples parsed: {}. Total samples discarded: {}. \n".format(total_data, mal_num))
 
 
+def data_preprocess_hug(filename, write):
+    with open(write, "w") as w:
+        with open(filename) as f:
+            ll = {}
+            mal_num = 0
+            for line in f:
+                line = json.loads(line)
+                line = line["code"].split("Output")
+                print(line)
+                # line[0] = line[0].replace("###", "")
+                # line[0] = line[0].replace("Instruction", "")
+                # line[0] = line[0].strip()
+                if len(line) != 2:
+                    mal_num += 1
+                    continue
+
+                instruction = line[0].split(":")
+                instruction = instruction[1]
+                instruction = instruction.replace("###", "")
+                instruction = instruction.strip()
+
+                output = line[1][2:]
+                # print(instruction, output)
+
+        print(mal_num)
+
+
+def generate_data_mosaic(filename, write):
+    print("Parsing " + filename)
+    with open(write, "w") as w:
+        with open(filename) as f:
+            mal_num = 0
+            data = []
+            sep = "Output"
+            for line in f:
+                line = json.loads(line)
+                line = line["code"].split(sep)
+                # instruction = line[0].split("Instruction': ")[1] # extract the instruction
+                instruction = line[0]
+                # after splitting, try to extract the output, if the string is malformed, ignore it
+                try:
+                    output = line[1]
+
+                    entry = {"prompt": instruction, "response": "###Output" + output.lstrip()}  # remove the white space in front
+                    w.write(str(entry)+"\n")
+                except IndexError:
+                    mal_num += 1
+            total_data = len(data)
+        print("Total samples parsed: {}. Total samples discarded: {}. \n".format(total_data, mal_num))
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--apibench_file", type=str, default=None, help="the ApiBench file with the data")
-    parser.add_argument("--output_file", type=str, default=None, help="the output file this script writes to")
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--apibench_file", type=str, default=None, help="the ApiBench file with the data")
+    # parser.add_argument("--output_file", type=str, default=None, help="the output file this script writes to")
+    # args = parser.parse_args()
 
+    # data_preprocess_hug("../data/apibench/huggingface_train.json", "../data/finetune/huggingface_train.json")
     # generate_data(args.apibench_file, args.output_file)
+    generate_data_mosaic("../data/apibench/huggingface_train.json", "../data/finetune/huggingface_train.jsonl")
+    generate_data_mosaic("../data/apibench/huggingface_eval.json", "../data/finetune/huggingface_eval.jsonl")
     # todo uncomment above and delete below
     # generate_data("../data/apibench/huggingface_eval.json", "../data/training/huggingface_eval.json")
     # generate_data("../data/apibench/huggingface_train.json", "../data/training/huggingface_train.json")
--- training/get-llama.py
+++ training/get-llama.py
@@ -2,8 +2,9 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, LlamaTokenizer
 
 # this scripted was used to get LLaMA weights and tokenizer
 if __name__ == "__main__":
-#    model = AutoModelForCausalLM.from_pretrained("huggyllama/llama-7b")
-#    model.save_pretrained("./llama")
-    tokenizer = LlamaTokenizer.from_pretrained("huggyllama/llama-7b", legacy=True)
+
+    # model = AutoModelForCausalLM.from_pretrained("huggyllama/llama-7b")
+    # model.save_pretrained("./llama")
+    tokenizer = LlamaTokenizer.from_pretrained("huggyllama/llama-7b", legacy=False)
     tokenizer.save_pretrained("./llama")
     # print(tokenizer.decode([424, 423]))g
